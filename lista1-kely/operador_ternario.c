8da3adf2542518eef3fc974d6636e56fcd8708ef
Mayrton <mayrtondias@hotmail.com>
2013-12-19 14:01:33 -0300

    operdor ternario  final

--- lista1-kely/operador_ternario.c
+++ lista1-kely/operador_ternario.c
@@ -2,11 +2,20 @@
 #include<stdlib.h>
 
 /*
+Esse codigo tem com objetivo mostrar o funcionamento do operador ternario
 */
 
 int main(){
 	
 	int a = 1, b = 2, c = -5, x = 0;
+	
+	/*observe que a instrucao abaixo tem tres operadores ternarios alinhados
+	onde podemos observar melhor usando os parentes dessa forma 
+	x = a < b ?( a < c ? a : c ): (b < c ? b : c);
+	ou seja, o perador ternario que esta no primeiro parenteses so' sera'
+	exercutado se o valor da variavel a for menor do que o valor da variavel b
+	caso contrario será exercutado operador ternario que se encontra no segundo 
+	parentes*/
 	
 	x = a < b ? a < c ? a : c : b < c ? b : c;
 	
