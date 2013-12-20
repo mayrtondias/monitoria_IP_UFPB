12ac6d0fc037f6d1e353987121863fb7a1255a0c
Mayrton <mayrtondias@hotmail.com>
2013-12-20 20:10:39 -0300

    encontrando erros

--- lista1-kely/encontrando_erros.c
+++ lista1-kely/encontrando_erros.c
@@ -16,4 +16,22 @@ int main(){
 	system("PAUSE");
 	return 0;
 }
+
+--------------------------------------------------------------------------
+ja no trecho abaixo foi declarado uma variavel do tipo float com o 
+identificador nomeado como int o que gerará um error, pois int e uma
+palavra reservada da linguagem e nao podemos usar essas palavras 
+como identificadores de constantes, de variaveis e nem funcoes.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int x, y;
+	float int = 5.23;
+	printf("%f", int);
+	system("PAUSE");
+	return 0;
+}
 */
