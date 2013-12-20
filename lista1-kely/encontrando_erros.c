a13be6dc837af9a76c21857875c9751d2fbddcde
Mayrton <mayrtondias@hotmail.com>
2013-12-20 14:36:45 -0300

    encontrado erros

---
+++ lista1-kely/encontrando_erros.c
@@ -0,0 +1,19 @@
+/*
+RESOLUCAO DO 4 QUESTAO DA 2° LISTA  
+IDENTIFICANDO OS ERROS NOS CODIGOS ABAIXO
+
+-----------------------------------------------------------------------------
+
+nesse trecho abaixo podemos notar que a variavel x esta sendo declarada
+duas vezes, o que causara um erro de compilacao, podemos notar tambem 
+que as variaveis x e y que foram declaradas não foram usadas podendo causar
+um aviso ao termino da compilação. 
+
+#include <stdlib.h>
+
+int main(){
+	int x, y, x;
+	system("PAUSE");
+	return 0;
+}
+*/
