725f7425746d8816e9d5583bbe241915a09e513f
Mayrton <mayrtondias@hotmail.com>
2013-12-22 18:30:15 -0300

    compara impresao 2

---
+++ lista1-kely/compara_impresao2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Nesse exemplo vamos analisar as saidas dos printf 
+dentro do laco de repeticao e observar o comportamento
+do mesmo. */
+
+int main(){
+	int x = 10;
+	
+	printf("A\n");
+	
+	/*Podemos notar ao analisar esse exemplo abaixo que
+	o operador -- tem maior precedencia em relacao ao 
+	operador >, fazendo com que o bloco do laco de repeticao
+	while nao seja executado nenhuma vez 
+	*/
+	
+	while (--x > 9){
+		printf("%d", x);
+	}
+	
+	printf("\nB\n");
+	
+	/*Ja nesse podemos notar que a condicao e a mesma,
+	no entanto, o bloco do laco de repeticao do-while
+	sera exercutado pelo menos uma vez caso a condicao
+	seja falsa, que é justamente o caso abaixo*/
+	
+	x = 10;
+	do {
+		printf("%d", x);
+	} while(--x > 9);
+	
+	return 0;
+}
