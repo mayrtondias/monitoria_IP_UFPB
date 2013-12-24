d52842f7372aa4adeee0f30135478299f5eed2fe
Mayrton <mayrtondias@hotmail.com>
2013-12-24 17:27:53 -0300

    Exercicio 12 final

--- lista1-kely/exercicio12.c
+++ lista1-kely/exercicio12.c
@@ -2,10 +2,9 @@
 #include<stdlib.h>
 
 /*Esse trecho de codigo ira solicitar um numero inteiro ao usuario,
-de tal forma, que ele ira calcular se o numero inserido e par ou impar.
-caso o numero seja para e programa mostrar o resultado do fatorial 
-desse numero, caso o numero seja impar ele mostrar o resultado da potencia
-do numero elevado a ele mesmo.
+de tal forma, que ele ira realizar os calculo para separar a unidade 
+de milhar, a centena, a dezena e a unidade, pois ao final dessa 
+operacoes, os digitos desse numero sera impresso de forma invertida.
 */
 
 int main(){
@@ -20,6 +19,7 @@ int main(){
         scanf("%f",&auxiliar);
     }while((auxiliar!=(int)auxiliar)||(auxiliar<0000)||(auxiliar>9999));
 	
+	//separando os digitos
 	numero=auxiliar;
 	uMilhar=numero/1000;
 	resto=numero%1000;
@@ -29,7 +29,6 @@ int main(){
 	unidade=resto%10;
 	
 	printf("O numero digitado foi: %d e sua inversao %d\n",numero,(unidade*1000+dezena*100+centena*10+uMilhar));
-	
 	
 	return 0;
 }
