3ce406d0ac648001e99cf4e0dc22ace7fa2b2770
Mayrton <mayrtondias@hotmail.com>
2013-12-23 21:16:16 -0300

    Exercicio 13

---
+++ lista1-kely/exercicio13.c
@@ -0,0 +1,51 @@
+/*Codigo disponibilizado pela professora Kely Diana Villacorta Villacorta
+da Universidade Federal da Paraiba
+*/
+
+//Exercício 13
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float la, lb,lc;
+    do{
+      printf("Dig. o primeiro lado: ");
+      scanf("%f",&la);
+
+    }while(la<=0);
+    do{
+      printf("Dig. o segundo lado: ");
+      scanf("%f",&lb);
+
+    }while(lb<=0);
+    do{
+      printf("Dig. o terceiro lado: ");
+      scanf("%f",&lc);
+
+    }while(lc<=0);
+
+
+    if((la+lb >=lc)&&(la+lc >=lb)&&(lc+lb >=la)){
+
+        printf("Triangulo VALIDO!!\n");
+
+        if(la==lb&& lb==lc){
+            printf("Triangulo EQUILATERO!!\n");
+        }else{
+            if( la==lb || la==lc || lc==lb){
+                 printf("Triangulo ISOSCELES!!\n");
+            }else{
+             printf("Triangulo ESCALENO!!\n");
+             }
+
+        }
+    }else{
+    printf("Triangulo INVALIDO!!\n");
+    }
+
+
+	system("pause");
+	return 0;
+}
