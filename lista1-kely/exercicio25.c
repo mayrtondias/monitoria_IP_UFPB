0aedc35737696e7ec86822f266020203e355b700
Mayrton <mayrtondias@hotmail.com>
2013-12-23 21:08:30 -0300

    Exercicio 25

---
+++ lista1-kely/exercicio25.c
@@ -0,0 +1,35 @@
+/*Codigo disponibilizado pela professora Kely Diana Villacorta Villacorta
+da Universidade Federal da Paraiba
+*/
+
+//Exercício 25
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float n_f;
+  int n,l,i,j,k;
+  do{
+    printf("Dig. um numero inteiro positivo: ");
+    scanf("%f",&n_f);
+  } while(n_f<=0 || n_f!=(int)n_f);
+  n=(int)n_f;
+
+  j=1;
+  k=1;
+  for(l=1; l<=n;l++){
+    for(i=j;i<=k ;i++){
+       printf("%d\t",i);
+
+    }
+    printf("\n");
+    j=i;
+    k=i+l;
+
+  }
+
+  system("pause");
+  return 0;
+}
