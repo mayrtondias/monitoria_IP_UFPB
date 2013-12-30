44b94d9936f79dc8858d91a246479f54258a5e00
mayrtondias <mayrtondias@hotmail.com>
2013-12-30 00:25:20 -0200

    Update melhoramento_1.c

--- Kely/lista1-kely/melhoramento_1.c
+++ Kely/lista1-kely/melhoramento_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*ao objetivo desse codigo e chamar atencao ao programador
+/*O objetivo desse codigo e chamar atencao ao programador
 sobre a String de formatacao da funcao scanf.
 */
 
@@ -14,13 +14,13 @@ int main(){
 	"%d\n", ou seja, a funcao scanf vai esperar que o usuario 
 	digite um numero inteiro e um \n (tecla ENTER)no formato que
 	foi passado, fazendo com que apos digitar o numero a funcao 
-	irá esperar um novo ENTER como foi solicitado pelo programador. 
+	ira esperar um novo ENTER como foi solicitado pelo programador. 
 	*/
 	
 	printf("Introduza um numero");
 	scanf("%d\n", &n);
 	/*scanf("%d", &n); mais perciso, pois caso o programador nao 
-	informe o usuario como deve se digitado o mesmo nao saberá o 
+	informe o usuario como deve se digitado o mesmo nao saberï¿½ o 
 	que deve digitar no programa*/
 	
 	printf("O numero eh = %d\n",n);
