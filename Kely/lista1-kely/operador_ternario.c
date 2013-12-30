2189c70384daa7a4ca5795234a2145eb06ab11cd
mayrtondias <mayrtondias@hotmail.com>
2013-12-30 00:40:43 -0200

    Update operador_ternario.c

--- Kely/lista1-kely/operador_ternario.c
+++ Kely/lista1-kely/operador_ternario.c
@@ -10,12 +10,12 @@ int main(){
 	int a = 1, b = 2, c = -5, x = 0;
 	
 	/*observe que a instrucao abaixo tem tres operadores ternarios alinhados
-	onde podemos observar melhor usando os parentes dessa forma 
+	onde podemos observar melhor usando os parentes dessa forma:
 	x = a < b ?( a < c ? a : c ): (b < c ? b : c);
 	ou seja, o perador ternario que esta no primeiro parenteses so' sera'
 	exercutado se o valor da variavel a for menor do que o valor da variavel b
 	caso contrario ser� exercutado operador ternario que se encontra no segundo 
-	parentes
+	parentes.
 	*/
 	
 	x = a < b ? a < c ? a : c : b < c ? b : c;
