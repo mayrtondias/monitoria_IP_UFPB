31e1397111aec6149df2bd56b96d5171a62c36dd
mayrtondias <mayrtondias@hotmail.com>
2014-01-08 00:17:47 -0200

    Update exercicio_2.c

--- Giorgia/Laboratorio_Exercicio_1/exercicio_2.c
+++ Giorgia/Laboratorio_Exercicio_1/exercicio_2.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Exercicio 02 - Fazer um programa que obtenha dois números inteiros, x e
-y, mostre o quociente e o resto da divisão inteira entre eles.
+/* Exercicio 02 - Fazer um programa que obtenha dois nï¿½meros inteiros, x e
+y, mostre o quociente e o resto da divisï¿½o inteira entre eles.
 */
 
 /*Nesse exercicio podemos armazenar os dois valores inseridos pelo usuario,
-e logo apos mostrar os valores das divisão e modulo(resto) pelo printf
+e logo apos mostrar os valores das divisï¿½o e modulo(resto) pelo printf
 */
 
 int main(){
