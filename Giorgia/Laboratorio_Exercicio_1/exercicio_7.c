9c20f9eca681dd0623272bbc506baf03bcc57f98
mayrtondias <mayrtondias@hotmail.com>
2014-01-09 00:44:42 -0200

    Update exercicio_7.c

--- Giorgia/Laboratorio_Exercicio_1/exercicio_7.c
+++ Giorgia/Laboratorio_Exercicio_1/exercicio_7.c
@@ -6,7 +6,7 @@ y, mostre o quociente e o resto da divis�o inteira entre eles.
 */
 
 /*Nesse exercicio podemos armazenar os dois valores inseridos pelo usuario,
-e logo apos mostrar os valores das divis�o e modulo(resto) pelo printf
+e logo apos mostrar os valores das divisao e modulo(resto) pelo printf
 */
 
 int main(){
