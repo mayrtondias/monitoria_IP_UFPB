3c2c0b19ea31fd999f3023111b7991de942fd20f
Mayrton <mayrtondias@hotmail.com>
2014-01-11 21:29:18 -0300

    update 7

--- Giorgia/Laboratorio_Exercicio_1/exercicio_7.c
+++ Giorgia/Laboratorio_Exercicio_1/exercicio_7.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Exercicio 07 - Fazer um programa que obtenha dois numeros inteiros, x e
+/* Exercicio 07 - Fazer um programa que obtenha dois n�meros inteiros, x e
 y, mostre o quociente e o resto da divisao inteira entre eles.
 */
 
-/*Nesse exercicio podemos armazenar os dois valores inseridos pelo usuario,
+/* Nesse exercicio podemos armazenar os dois valores inseridos pelo usuario,
 e logo apos mostrar os valores das divisao e modulo(resto) pelo printf
 */
 
