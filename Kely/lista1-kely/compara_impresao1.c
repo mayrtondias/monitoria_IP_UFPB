034f8c2c96b3bc22521aa674d716dc3dc8e05d4f
Mayrton <mayrtondias@hotmail.com>
2014-01-13 12:34:25 -0300

    Update Compara impresao 1

--- Kely/lista1-kely/compara_impresao1.c
+++ Kely/lista1-kely/compara_impresao1.c
@@ -2,8 +2,9 @@
 #include<stdlib.h>
 
 /* Nesse exemplo vamos analisar as saidas dos printf 
-dentro do laco de repeticao e observar o comportamento
-do mesmo. */
+dentro de um laco de repeticao e observar o comportamento
+do mesmo. 
+*/
 
 int main(){
 	
@@ -11,29 +12,31 @@ int main(){
 	
 	printf("A\n");
 	
-	/*nesse laco podemos observar que o valor da 
+	/* Nesse laco podemos observar que o valor da 
 	variavel i vai incrementando em duas unidades,
-	que sera a sequinte sequencia
+	e tera a sequinte sequencia
 	1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21. 
-	sendo que ao imprimir o valor da variavel i ha
+	sendo que, ao imprimir o valor da variavel i, ha
 	um trucamento da parte decimal, visto que, a 
-	mesma e uma variavel do tipo int 
+	mesma e' uma variavel do tipo int 
 	*/
 	
-	for (i = 1 ; i <=20; i+=2)
-	printf("\n%d", i/2);
+	for (i = 1 ; i <=20; i+=2){
+		printf("\n%d", i/2);
+	}
 	
 	printf("\n\nB\n");
 	
-	/*Ja nesse trecho e bem mais simples de observar que 
+	/* Ja nesse trecho e bem mais simples de observar que 
 	o valor da variavel segue a seguinte sequencia:
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10. 
 	Nesse caso o valor 10 e impresso, pois a condicao e 
 	verdadeira para os valores menores ou iguais a 10.
 	*/
 	
-	for (j = 0 ; j <= 10; j++)
-	printf("\n%d", j);
+	for (j = 0 ; j <= 10; j++){
+		printf("\n%d", j);
+	}
 	
 	return 0;
 }
