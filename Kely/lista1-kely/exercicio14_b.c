372b7e50c6f49c246ffe280bd18ea991fc3ce944
Mayrton <mayrtondias@hotmail.com>
2014-01-14 20:22:07 -0300

    Update Exercicio 14 b

--- Kely/lista1-kely/exercicio14_b.c
+++ Kely/lista1-kely/exercicio14_b.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Esse trecho de codigo ira solicitar dois numeros
+/* Esse trecho de codigo ira solicitar dois numeros
 inteiro positivo ao usuario.Ao armazenar os dois valores
 sera feita uma comparacao se A<=B entao o programa ira
 soma todos os numeros pares entre os mesmos
