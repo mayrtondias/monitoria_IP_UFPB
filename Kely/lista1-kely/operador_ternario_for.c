08e44146ba9d5604e5aa7784663ce3c7bfe92cbb
Mayrton <mayrtondias@hotmail.com>
2014-01-16 11:33:52 -0300

    Update Operador Ternario for

--- Kely/lista1-kely/operador_ternario_for.c
+++ Kely/lista1-kely/operador_ternario_for.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Esse codigo tem com objetivo mostrar o funcionamento 
+/* Esse codigo tem com objetivo mostrar o funcionamento 
 do operador ternario, so que nesse caso foi tambem 
-incluido um laco de repeticao for */
+incluido um laco de repeticao for 
+*/
 
 int main(){
 	
 	int x;
 	
-	/*observe os valores da variavel x em cada interacao
-	1� interacao:
+	/*Observe os valores da variavel x em cada interacao
+	primeira interacao:
 	antes:
 	o valor de x e igual a 0
 	imprime:
@@ -18,7 +19,7 @@ int main(){
 	depois:
 	o valor de x e igual a 2
 	
-	2� interacao:
+	segunda interacao:
 	antes:
 	o valor de x e igual a 2
 	imprime:
@@ -26,7 +27,7 @@ int main(){
 	depois:
 	o valor de x e igual a  4
 	
-	3� interacao:
+	terceira interacao:
 	antes:
 	o valor de x e igual a 4
 	imprime:
@@ -34,7 +35,7 @@ int main(){
 	depois:
 	o valor de x e igual a 1
 	
-	4� interacao:
+	quarta interacao:
 	antes:
 	o valor de x e igual a 1
 	imprime:
@@ -42,7 +43,7 @@ int main(){
 	depois:
 	o valor de x e igual a 3
 	
-	5� interacao:
+	quinta interacao:
 	antes:
 	o valor de x e igual a 3
 	imprime:
@@ -50,13 +51,14 @@ int main(){
 	depois:
 	o valor de x e igual a 5
 	
-	Antes da 6� interacao para, pois o valor de x e maior que 4
+	Antes da sexta interacao para, pois o valor de x e maior que 4
 	*/
 	
 	for (x=0;x <=4; x++){
 		x=x!=4? x+1:0;
 		printf("\n%d",x);	
 	}
+	
 	return 0;
 }
 
