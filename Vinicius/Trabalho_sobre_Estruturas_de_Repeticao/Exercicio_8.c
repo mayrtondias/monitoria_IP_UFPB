a18a56af5dd1550296c906480494167f22810c8a
Mayrton <mayrtondias@hotmail.com>
2014-01-16 10:32:03 -0300

    Update Exercicio 8

--- Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_8.c
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_8.c
@@ -14,5 +14,49 @@ Finalize a entrada de dados com a digitacao do numero 30.000.
 
 int main(){
 	
+	int teste=0;
+	int numero;
+	int somaTotal=0;
+	int qtdeNumeros=0;
+	int maiorNumero=0;
+	int menorNumero=0;
+	int somaPares=0;
+	int qtdePares=0;
+	
+	while(teste==0){
+		printf("Digite um numero: \n");
+		scanf("%d",&numero);
+		
+		if(numero==30000){
+			printf("A soma dos numeros digitado foi: %d\n",somaTotal);
+			printf("A quantidade de numeros digitado foi: %d\n",qtdeNumeros);
+			printf("A media dos numeros digitado foi: %f\n",(float)(somaTotal/qtdeNumeros));
+			printf("O maior numero digitado foi: %d\n",maiorNumero);
+			printf("O menor numero digitado foi: %d\n",menorNumero);
+			printf("A media dos numeros pares digitado foi: %f\n",(float)(somaPares/qtdePares));
+			printf("A Porcentagem dos numeros impares foi: %f % \n",(float)(((qtdeNumeros-qtdePares)*100)/qtdeNumeros));
+			teste=1;
+			continue;
+		}
+		
+		somaTotal+=numero;
+		++qtdeNumeros;
+		if(qtdeNumeros==1){
+			maiorNumero=numero;
+			menorNumero=numero;
+		}else{
+			if(numero>maiorNumero){
+				maiorNumero=numero;
+			}
+			if(numero<menorNumero){
+				menorNumero=numero;
+			}
+		}
+		if((numero%2)==0){
+			somaPares+=numero;
+			++qtdePares;
+		}
+	}
+	
 	return 0;
 }
