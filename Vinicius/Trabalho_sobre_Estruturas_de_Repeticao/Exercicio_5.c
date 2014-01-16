eef2049348aa4e7cb3ef125c3518b8d854e4c494
Mayrton <mayrtondias@hotmail.com>
2014-01-15 21:13:22 -0300

    Update Exercicio 5

---
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*Construa um programa para encontrar o maior e o menor 
+número de uma série de números positivos fornecidos pelo 
+usuário através do teclado. O dado finalizador é o número 
+–1, e este não deve ser considerado na análise dos 
+números da série.
+*/
+
+int main(){
+	
+	return 0;
+}
