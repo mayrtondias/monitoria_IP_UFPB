958039dac085b4765f25dc3fde5804de711c4b83
Mayrton <mayrtondias@hotmail.com>
2014-01-16 10:31:41 -0300

    Update Exercicio 6

--- Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_6.c
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_6.c
@@ -17,7 +17,7 @@ c) qual a media de acidentes de transito nas cidades com
 */
 
 int main(){
-	
+	int contador=0;
 	int codigoCidade;
 	int numVeiculoPasseio;
 	int acidentesVitimas;
@@ -26,9 +26,47 @@ int main(){
 	int cidadeMaiorAcidente;
 	int cidadeMenorAcidente;
 	float mediaVeiculos;
-	float mediaAcidentes;
+	float mediaAcidentes=0;
+	int contadorMediaAcidentes=0;
 	
+	while(contador<5){
+		
+		printf("Digite o codigo da cidade %d\n", (contador+1));
+		scanf("%d",&codigoCidade);
+		printf("Digite o numero de veiculos de passeio da cidade %d\n", (contador+1));
+		scanf("%d",&numVeiculoPasseio);
+		printf("Digite o numero de acidentes de transito com vitimas da cidade %d\n", (contador+1));
+		scanf("%d",&acidentesVitimas);
+		
+		if(contador==0){
+			qtdeMaiorAcidente=acidentesVitimas;
+			qtdeMenorAcidente=acidentesVitimas;
+	 		cidadeMaiorAcidente=codigoCidade;
+	 		cidadeMenorAcidente=codigoCidade;
+		}else{
+			if(acidentesVitimas>qtdeMaiorAcidente){
+				qtdeMaiorAcidente=acidentesVitimas;
+				cidadeMaiorAcidente=codigoCidade;
+			}
+			if(acidentesVitimas<qtdeMenorAcidente){
+				qtdeMenorAcidente=acidentesVitimas;
+				cidadeMenorAcidente=codigoCidade;
+			}
+		}
+		mediaVeiculos+=numVeiculoPasseio;
+		
+		if(numVeiculoPasseio<2000){
+			mediaAcidentes+=acidentesVitimas;
+			++contadorMediaAcidentes;
+		}
+		
+		++contador;
+	}
 	
-	
+	printf("O maior indice de acidentes e' %d da cidade %d\n",qtdeMaiorAcidente,cidadeMaiorAcidente);
+	printf("O menor indice de acidentes e' %d da cidade %d\n",qtdeMenorAcidente,cidadeMenorAcidente);
+	printf("A media de veiculos das cidades e' %f \n",(mediaVeiculos/5));
+	printf("A media de acidente de transito nas cidades \ncom menos de 2.000 veiculos de passeio e': %f \n",(mediaAcidentes/contadorMediaAcidentes));
+		
 	return 0;
 }
