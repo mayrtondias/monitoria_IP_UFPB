62719ccbcb9c5c4b8691cd2bce984f52c97db86d
Mayrton <mayrtondias@hotmail.com>
2014-01-15 21:13:48 -0300

    Update Exercicio 8

---
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*Escreva um programa que receba varios numeros, calcule e mostre:
+> a soma dos numeros digitados;
+> a quantidade de numeros digitados;
+> a media dos numeros digitados;
+> o maior numero digitado;
+> o menor numero digitado;
+> a media dos numeros pares;
+> a percentagem dos numeros impares entre todos os numeros digitados.
+Finalize a entrada de dados com a digitacao do numero 30.000.
+*/
+
+int main(){
+	
+	return 0;
+}
