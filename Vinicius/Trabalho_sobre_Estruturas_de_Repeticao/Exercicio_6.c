0394fe04a8f73e27573d96a866637c31b0dde1ff
Mayrton <mayrtondias@hotmail.com>
2014-01-15 21:13:29 -0300

    Update Exercicio 6

---
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Foi feita uma estatistica em cinco cidades brasileiras para
+ coletar dados sobre acidentes de transito. Foram obtidos os 
+ seguintes dados:
+a) codigo da cidade;
+b) numero de veiculos de passeio (em 2007);
+c) numero de acidentes de transito com vitimas (em 2007).
+	>Deseja-se saber:
+a) qual o maior e o menor indice de acidentes de transito e 
+   a que cidades pertencem;
+b) qual e media de veiculos nas cinco cidades juntas;
+c) qual a media de acidentes de transito nas cidades com 
+   menos de 2.000 veiculos de passeio.
+	>Assuma que os dados informados sejam validos.
+*/
+
+int main(){
+	
+	return 0;
+}
