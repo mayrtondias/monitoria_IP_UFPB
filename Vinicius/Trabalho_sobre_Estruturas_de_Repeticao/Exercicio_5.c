fa84cf569e04436982dbbec76b2e55cdc2b83af1
Mayrton <mayrtondias@hotmail.com>
2014-01-15 22:47:30 -0300

    Update Exercicio 5

--- Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_5.c
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_5.c
@@ -10,5 +10,35 @@ números da série.
 
 int main(){
 	
+	int teste=0;
+	int numero;
+	int maior=0;
+	int menor=0;
+	
+	do{
+		printf("Digite um numero: \n");
+		scanf("%d", &numero);
+		
+		if(numero<=0){
+			if(numero==-1){
+				teste=1;
+			}
+			continue;
+		}
+		if(maior==0){
+			maior=numero;
+			menor=numero;
+		}else{
+			if(numero>maior){
+				maior=numero;
+			}
+			if(numero<menor){
+				menor=numero;
+			}
+		}
+	}while(teste==0);
+	
+	printf("O Valor do maior numero e: %d\nO Valor do menor numero e: %d\n", maior, menor);
+	
 	return 0;
 }
