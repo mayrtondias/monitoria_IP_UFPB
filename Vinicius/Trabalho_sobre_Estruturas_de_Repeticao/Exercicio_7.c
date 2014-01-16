3cdae2a967c7f8d9b04ea075d2ba490c1780b13b
Mayrton <mayrtondias@hotmail.com>
2014-01-15 21:13:35 -0300

    Update Exercicio 7

---
+++ Vinicius/Trabalho_sobre_Estruturas_de_Repeticao/Exercicio_7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* Em um campeonato de futebol existem cinco times e cada um 
+possui onze jogadores. Faca um programa que receba a idade, 
+o peso e a altura de cada um dos jogadores, calcule e mostre:
+> a quantidade de jogadores com idade inferior a 18 anos;
+> a media das idades dos jogadores de cada time;
+> a media das alturas de todos os jogadores do campeonato;
+> a percentagem de jogadores com mais de 80 quilos entre 
+  todos os jogadores do campeonato.
+   Assuma que os dados informados sejam validos.
+*/
+
+int main(){
+	 
+	return 0;
+}
