c0b810ac87f37ce808d70dd6d1575055d0b03188
Mayrton <mayrtondias@hotmail.com>
2014-01-16 11:33:28 -0300

    Update melhoramento 2

--- Kely/lista1-kely/melhoramento_2.c
+++ Kely/lista1-kely/melhoramento_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Nesse codigo abaixo sera mostrado um erro de compatibilidade
+/* Nesse codigo abaixo sera mostrado um erro de compatibilidade
 com relacao aos tipos das variaveis e a String de formatacao, 
 nas funcao printf e scanf.
 */
@@ -10,11 +10,12 @@ int main(){
 	
 	int n;
 	
-	/*podemos notar que nesse exemplo foi declarada uma variavel
+	/* Podemos notar que nesse exemplo foi declarada uma variavel
 	do tipo int, mas na chamanda da funcao scanf foi passado na 
 	string de formatacao %f no lugar do %i, o que causa um efeito
 	no valor armazenado e mostrado, nao esperado pelo programador.
 	*/
+	
 	printf("Introduza um numero");
 	scanf("%f", &n);
 	printf("O numero eh = %f\n",n);
